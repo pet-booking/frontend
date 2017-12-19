d93cd60cff72c93a03d66b6fe0357e00c4c2d700
Ron Barrantes <ronbarrantes@gmail.com>
2017-12-19 14:53:13 -0800

    Initial heroku dependency

--- webpack.config.js
+++ webpack.config.js
@@ -2,97 +2,29 @@
 
 require('dotenv').config()
 
-const {DefinePlugin, EnvironmentPlugin} = require('webpack')
-const CleanPlugin = require('clean-webpack-plugin')
-const UglifyPlugin = require('uglifyjs-webpack-plugin')
-const HTMLPlugin = require('html-webpack-plugin')
-const ExtractPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const production = process.env.NODE_ENV === 'production'
-
-let plugins = [
-  new HTMLPlugin({title: 'Sail Point'}),
-  new ExtractPlugin('bundle.[hash].css'),
-  new DefinePlugin({
-    __API_URL__: JSON.stringify(process.env.API_URL),
-  }),
-  new EnvironmentPlugin({
-    NODE_ENV: process.env.NODE_ENV,
-  }),
-]
-
-if(production){
-  plugins = plugins.concat([
-    new UglifyPlugin(),
-    new CleanPlugin(),
-  ])
-}
+const plugins = [  new HtmlWebpackPlugin({
+  title: 'Pet Booking',
+  template: `${__dirname}/public/index.html`,
+}),]
 
 module.exports = {
   plugins,
-  devtool: production ? undefined : 'source-map',
   entry: `${__dirname}/src/main.js`,
-  devServer: {
-    historyApiFallback: true,
-  },
+
   output: {
-    filename: 'bundle.[hash].js',
-    path: `${__dirname}/build`,
+    filename: 'bundle.js',
+    path: `${__dirname}/dist`,
     publicPath: process.env.CDN_URL,
   },
   module: {
     rules: [
       {
-        test: /\.(woff|woff2|ttf|eot).*/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              name: 'font/[name].[hash].[ext]',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(jpg|gif|png|svg)$/,
-        exclude: /\.icon\.svg$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'image/[name].[hash].[ext]',
-          },
-        }],
-      },
-      {
-        test: /\.icon\.svg$/,
-        loader: 'raw-loader',
-      },
-      {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env'],
-        },
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractPlugin.extract({
-          use: [
-            'css-loader',
-            'resolve-url-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: production ? false : true,
-                includePaths: [`${__dirname}/src/style`],
-              },
-            },
-          ],
-        }),
+        loader: ['babel-loader'],
       },
     ],
-  },
+  }
 }
