1c09a6cf9d60ebde937bb161d3d4c8b23c8421f7
ronbarrantes <ronbarrantes@gmail.com>
2019-01-12 23:41:16 -0800

    Use hash for bundles

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,28 @@
 
 require('dotenv').config()
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const plugins = [  new HtmlWebpackPlugin({
-  title: 'Pet Booking',
-  template: `${__dirname}/public/index.html`,
-}),]
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+
+const production = process.env.NODE_ENV === 'production'
+
+const plugins = [  
+  new HtmlWebpackPlugin({
+    template: `${__dirname}/public/index.html`,
+  }),
+  new CleanWebpackPlugin(['dist']),
+]
 
 module.exports = {
   plugins,
+  devtool: production ? undefined : 'source-map',
+  mode: process.env.NODE_ENV,
   entry: `${__dirname}/src/main.js`,
 
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
     path: `${__dirname}/dist`,
     publicPath: process.env.CDN_URL,
   },
