ef3d1ccfb6c146ef65692717a7987a0d103a6ba7
ronbarrantes <ronbarrantes@gmail.com>
2019-01-14 01:35:02 -0800

    Add Home and Nav components

---
+++ src/components/nav/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/pet">Pet</Link></li>
+      <li><Link to="/profile">Profile</Link></li>
+      <li><Link to="/signup">SignUp</Link></li>
+      <li><Link to="/signin">SignIn</Link></li>
+    </ul>
+  </nav>
+)
+
+export default Nav
