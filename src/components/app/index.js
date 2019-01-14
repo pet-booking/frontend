00116e34c217d9e4297055e08b81ab1072b5c72d
ronbarrantes <ronbarrantes@gmail.com>
2019-01-14 01:47:54 -0800

    Add routes to the app component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Pet from '../pet'
+import Nav from '../nav'
+import Home from '../home'
 import Profile from '../profile'
-import SignIn from '../sign-in'
-import SignUp from '../sign-up'
+import SignIn from '../signin'
+import SignUp from '../signup'
 
 const App = () => (
   <Router>
     <div>
       <h1>hello world</h1>
-      <Pet />
-      <Profile />
-      <SignIn />
-      <SignUp />
+      <Nav />
+      <Route exact path="/" component={Home} />
+      <Route path="/pet" component={Pet} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
     </div>
   </Router>
 )
