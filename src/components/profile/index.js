d17e1f6c1bd77ec6be83d6c7f32a6dc746476960
ronbarrantes <ronbarrantes@gmail.com>
2019-01-13 23:46:59 -0800

    Add multiple initial components
    Pet
    Profile
    SignIn
    SignUp

---
+++ src/components/profile/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = () => (
+  <div>
+    <p>Profile Route</p>
+  </div>
+)
+
+export default Profile
---
+++ src/components/signup/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => (
+  <div>
+    <p>SignUp Route</p>
+  </div>
+)
+
+export default SignUp
