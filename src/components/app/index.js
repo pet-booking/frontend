4916eb1c3aa682fb2f9702a95fa14446f230776e
ronbarrantes <ronbarrantes@gmail.com>
2019-01-13 23:51:36 -0800

    Add components to App

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import Pet from '../pet'
+import Profile from '../profile'
+import SignIn from '../sign-in'
+import SignUp from '../sign-up'
+
 const App = () => (
   <Router>
     <div>
       <h1>hello world</h1>
+      <Pet />
+      <Profile />
+      <SignIn />
+      <SignUp />
     </div>
   </Router>
 )
--- src/components/signin/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SignIn = () => (
-  <div>
-    <p>SignIn Route</p>
-  </div>
-)
-
-export default SignIn
